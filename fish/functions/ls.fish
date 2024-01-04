7b5e64d52c0f90a67f934446724908be9740dbad
Benjamin Ulmer <benjaminulmer55@gmail.com>
2024-01-03 20:59:42 -0700

    Add way more stuff; tracking directly from .config

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,4 +1,4 @@
-function ls --wraps=exa --description 'alias ls=exa'
-  exa $argv
+function ls --wraps=exa --wraps=lsd --description 'alias ls=lsd'
+  lsd $argv
         
 end
--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ sudo apt update
 sudo apt install fish
 
 sudo apt install btop
-sudo apt install kitty
 sudo apt install bat
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install jethrokuan/z
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 curl https://sh.rustup.rs -sSf | sh
 fish_add_path /home/benjaminulmer/.cargo/bin/
@@ -18,3 +19,7 @@ cargo install lsd
 
 mkdir -p ~/.local/share/fonts
 cp RobotoMono/*.ttf ~/.local/share/fonts/
+
+fish_add_path /home/benjaminulmer/.local/kitty.app/bin/
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emula
+tor /home/benjaminulmer/.local/kitty.app/bin/kitty 50
